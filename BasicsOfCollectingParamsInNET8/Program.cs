37d21fa24631052f7a6c39a144f8e132a968d2bb
ДеЮра <doomov@yandex.ru>
2025-02-09 18:35:33 +0700

    Add files via upload

---
+++ BasicsOfCollectingParamsInNET8/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace BasicsOfCollectingParamsInNET8
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Hello, .NET8!");
+			Employee employees = new Employee(new List<string> { "Potter", "Vovan" });
+			employees._employees.AddRange(new List<string> { "Poc", "NaN" });
+
+			foreach (var item in employees._employees)
+			{
+				Console.WriteLine(item);
+			}
+			Console.ReadKey();
+		}
+
+
+
+		public class Employee
+		{
+			public List<string> _employees;
+
+			public Employee(List<string> employees)
+			{
+				_employees = employees;
+			}
+		}
+	}
+}
---
+++ BasicsOfCollectingParamsInNET9/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace BasicsOfCollectingParamsInNET9
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, .NET9!");
+			Employee employees = new Employee("Potter", "Vovan");
+			employees._employees.AddRange("Poc", "NaN");
+
+			foreach (var item in employees._employees)
+			{
+				Console.WriteLine(item);
+			}
+			Console.ReadKey();
+		}
+
+		public class Employee
+		{
+			public List<string> _employees;
+
+			public Employee(params List<string> employees)
+			{
+				_employees = employees;
+			}
+		}
+	}
+}
